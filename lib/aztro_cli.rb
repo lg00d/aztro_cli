002757be112c3284c1a4e80791c0a92496e97817
lg00d <egoodman1180@icloud.com>
2020-12-20 13:00:58 -0500

    imported relative project files

--- lib/aztro_cli.rb
+++ lib/aztro_cli.rb
@@ -1,6 +1,11 @@
-require "aztro_cli/version"
+require_relative "./aztro_cli/version"
 
-module AztroCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative './aztro_cli/api'
+require_relative './aztro_cli/cli'
+require_relative './aztrocli/horoscope'
+#a subset of require and is a convenient method to use when you are referring to a file 
+#that is relative to the current file you are working on (basically, within the same project directory).
+
+require 'rest-client'
+require 'json' 
+#is a method that is used when you want to reference and execute code that is not written in your current file
