fffd8a40c0821b0021ca10a34228938e95529da3
lg00d <egoodman1180@icloud.com>
2020-12-20 20:43:04 -0500

    fixed naming error

--- lib/aztro_cli.rb
+++ lib/aztro_cli.rb
@@ -2,7 +2,7 @@ require_relative "./aztro_cli/version"
 
 require_relative './aztro_cli/api'
 require_relative './aztro_cli/cli'
-require_relative './aztrocli/horoscope'
+require_relative './aztrocli/sunsign'
 
 
 require 'rest-client'
--- lib/aztro_cli/cli.rb
+++ lib/aztro_cli/cli.rb
@@ -1,19 +1,20 @@
 class AztroCli::CLI
     attr_accessor :user_sign
-    attr_reader :sunsign_objects
+    attr_reader :api_data
 
     def initialize
         sun_signs = ["aquarius", "pisces", "aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn"]
     
         sun_signs.each do |sign| 
             data = API.get_data(sign)
-           sunsign_objects = sunsign.new(name, data)
+           sunsign_object = sunsign.new(name, data)
+            api_data << sunsign_object
         end
         call
     end
 
     def call
-        puts "Welcome! "
+        puts "Welcome!"
         puts "To learn more about your sun sign, please enter 'start'"
         puts "To find your sun sign, please enter 'list'"
         puts "To exit, please enter 'exit'"
